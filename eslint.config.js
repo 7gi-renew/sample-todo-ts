087fdf46ec4651d8d5544072eeecc2b58b57d7bf
ichikawa <bbbb_vanilla@icloud.com>
2025-04-25 23:29:26 +0900

    yamlファイルにbuildの設定を追加

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-
+import pluginReactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReactJSXRuntime,
 ]);

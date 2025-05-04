6b59c47f7d42eb3a76cbc454c5866e914389135f
ichikawa <bbbb_vanilla@icloud.com>
2025-05-04 23:38:49 +0900

    テスト条件を変更

--- src/tests/AppComponent.spec.tsx
+++ src/tests/AppComponent.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 // import userEvent from '@testing-library/user-event'
 import "@testing-library/jest-dom";
 import App from "../App";
@@ -6,6 +6,8 @@ import App from "../App";
 describe("テスト1", () => {
   test("タイトルが存在している", async () => {
     render(<App />);
-    expect(screen.getByText("学習記録アプリ")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText("学習記録アプリ")).toBeInTheDocument();
+    });
   });
 });
--- src/utils/Supabase.ts
+++ src/utils/Supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const Supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  process.env.VITE_SUPABASE_URL as string,
+  process.env.VITE_SUPABASE_ANON_KEY as string
 );
